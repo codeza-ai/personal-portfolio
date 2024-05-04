705d6f99baca4a85933889f2ada022367bfc32fc
codeza-ai <darshan.odedara@gmail.com>
2024-05-04 10:58:40 +0530

    04/04/24 - Created pages and components- Header and Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='about'></div>
+        <div className='social-medias'></div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='searchBox'>
+
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header

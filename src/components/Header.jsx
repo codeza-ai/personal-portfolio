a9c7b86c95fc4f7e2276ff3d748fc67dd6b5a9b7
codeza-ai <darshan.odedara@gmail.com>
2024-05-06 09:54:53 +0530

    Trying to host on GitHub pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,150 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../Header.css"
+// import React from 'react'
+import { useState } from "react";
+import "./Header.css"
 
-const Header = () => {
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+
+// const pages = ["Home", "Skills", "Projects", "Contact"];
+const pages = [{name: "Home",
+                to: "/"
+              },
+              {
+                name: "Skills",
+                to: "/#skills"
+              },
+              {
+                name: "Projects",
+                to: "/#projects"
+              },
+              {
+                name: "Contact",
+                to: "/contact"
+              }];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <header>
-      <div className='searchBox'>
-        <input type="text" />
-        <button>Search</button>
-      </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="/#skills">Skills</a></li>
-          <li><a href="/#projects">Projects</a></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
+    <AppBar position="static" sx={{
+        backgroundColor: '#0C2D57',
+        color: "#EFECEC",
+        fontFamily: "Poppins"
+      }}>
+      <Container maxWidth="xl" >
+        <Toolbar disableGutters>
 
-export default Header
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+            <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' }
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'inherit', display: 'block', padding: '10px 50px' }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Collaborate">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Darshan" src="public\Profile.jpg" sx={
+                  {
+                    width: '55px',
+                    height: "auto"
+                  }
+                }/>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '55px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <MenuItem key="collab" onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{`Let's build >`}</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;
